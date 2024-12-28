aff0b67e99323a60239de52e4b358f57df680c91
areumYang <yangareum1818@nate.com>
2024-12-28 13:56:47 +0900

    주석제거 및 모바일 화면 프로필 nav 스타일 수정

--- src/lib/actions/profileAction.ts
+++ src/lib/actions/profileAction.ts
@@ -86,8 +86,6 @@ export async function updateUserData(id: string, updateDoc: UpdateDocument) {
   await connectDB();
 
   const check = await User.exists({ _id: id });
-  console.log({ exist: check });
-
   try {
     const update = await User.findOneAndUpdate({ _id: id }, updateDoc, {
       new: true,
@@ -96,7 +94,6 @@ export async function updateUserData(id: string, updateDoc: UpdateDocument) {
     if (!update) {
       return { state: false, message: "해당 프로필을 찾을 수 없습니다." };
     }
-    console.log("update", { update });
 
     revalidatePath("/my/profile");
 
@@ -105,7 +102,6 @@ export async function updateUserData(id: string, updateDoc: UpdateDocument) {
       message: "프로필 정보가 변경되었습니다.",
     };
   } catch (error: any) {
-    console.log("get profile" + error);
     return { state: false, message: "프로필 정보 로딩에 실패했습니다." };
   }
 }
