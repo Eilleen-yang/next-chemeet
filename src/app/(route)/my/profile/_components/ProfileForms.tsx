6e86a41afc559b059d25de2203060de78f7f5cc2
areumYang <yangareum1818@nate.com>
2024-12-30 23:24:27 +0900

    [Chore] 프리패치 수정

--- src/app/(route)/my/profile/_components/ProfileForms.tsx
+++ src/app/(route)/my/profile/_components/ProfileForms.tsx
@@ -8,7 +8,11 @@ import FormUpdatePhoneNumber from "./UpdatePhoneNumber/FormUpdatePhoneNumber";
 import DeleteAccountConfirm from "./DeleteAccountConfirm";
 import { getUserData } from "@/lib/actions/userAction";
 import { ProfileSchema } from "@/types/model/User";
-import { QueryClient } from "@tanstack/react-query";
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
 
 export default async function ProfileForms() {
   const queryClient = new QueryClient();
@@ -16,28 +20,41 @@ export default async function ProfileForms() {
   const userId = session?.user.id as string;
   const sessionProvider = session?.account.provider;
 
+  if (userId) {
+    await queryClient.prefetchQuery({
+      queryKey: ["profile", userId],
+      queryFn: () => getUserData(userId),
+    });
+  } else {
+    return;
+  }
+
+  const dehydratedState = dehydrate(queryClient);
+
   return (
-    <div className="gridContent grid xl:grid-cols-[5fr_4fr] xl:items-start gap-gutter-xl">
-      <div className="flex flex-col gap-8">
-        <p className="text-H2 text-label-dimmed">{session?.user.name}</p>
-        <FormEditProfileImageWithPreview userId={userId} />
-        <div className="w-full h-[1px] border-t border-t-line-normal"></div>
-        <FormEditProfile userId={userId} />
-        {sessionProvider === "credentials" && (
-          <>
-            <div className="w-full h-[1px] border-t border-t-line-normal"></div>
-            <SectionTitle size="md">비밀번호 변경</SectionTitle>
-            <FormUpdatePassword />
-            <div className="w-full h-[1px] border-t border-t-line-normal"></div>
-            <SectionTitle size="md" className="mb-2">
-              연락처 수정
-            </SectionTitle>
-            <FormUpdatePhoneNumber userId={userId} />
-            <div className="w-full h-[1px] border-t border-t-line-normal"></div>
-            <DeleteAccountConfirm />
-          </>
-        )}
+    <HydrationBoundary state={dehydrate}>
+      <div className="gridContent grid xl:grid-cols-[5fr_4fr] xl:items-start gap-gutter-xl">
+        <div className="flex flex-col gap-8">
+          <p className="text-H2 text-label-dimmed">{session?.user.name}</p>
+          <FormEditProfileImageWithPreview userId={userId} />
+          <div className="w-full h-[1px] border-t border-t-line-normal"></div>
+          <FormEditProfile userId={userId} />
+          {sessionProvider === "credentials" && (
+            <>
+              <div className="w-full h-[1px] border-t border-t-line-normal"></div>
+              <SectionTitle size="md">비밀번호 변경</SectionTitle>
+              <FormUpdatePassword />
+              <div className="w-full h-[1px] border-t border-t-line-normal"></div>
+              <SectionTitle size="md" className="mb-2">
+                연락처 수정
+              </SectionTitle>
+              <FormUpdatePhoneNumber userId={userId} />
+              <div className="w-full h-[1px] border-t border-t-line-normal"></div>
+              <DeleteAccountConfirm />
+            </>
+          )}
+        </div>
       </div>
-    </div>
+    </HydrationBoundary>
   );
 }
